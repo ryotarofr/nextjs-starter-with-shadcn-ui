6962993761019abbb625c7467aaa66b373a21d86
ryotarofr <ryoryo.fr0608@icloud.com>
2023-11-19 12:30:27 +0900

    delete files

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Editor } from '@/components/Editor/text-editor'
 import ToggleThemeButton from '@/components/ui/toggle-theme'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <ToggleThemeButton />
-      <Editor />
     </div>
   )
 }
